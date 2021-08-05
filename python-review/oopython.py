9995446067dc30545ada893eac8805bc4da0bfce
Sandra Coburn <sandracoburn@ezs-macbook-pro.lan>
2021-08-04 18:21:44 -0700

    Dunder methods being modyfied inside a class

--- python-review/oopython.py
+++ python-review/oopython.py
@@ -151,9 +151,41 @@ for char in [wizard1,archer1]:
 #Instrospection -> the ability to determine the type of an object at runtime -> dir
 print(dir(wizard1))
 '''
+Dunder methods: 
+
 ['__class__', '__delattr__', '__dict__', '__dir__', '__doc__', '__eq__', 
 '__format__', '__ge__', '__getattribute__', '__gt__', '__hash__', '__init__',
  '__init_subclass__', '__le__', '__lt__', '__module__', '__ne__', '__new__',
   '__reduce__', '__reduce_ex__', '__repr__', '__setattr__', '__sizeof__', '__str__',
    '__subclasshook__', '__weakref__', 'attack', 'email', 'name', 'power', 'sign_in']
 '''
+#Dunder methods can be modified within a class
+class Toy():
+  def __init__(self,color,age):
+    self.color = color
+    self.age = age
+    self.my_dict = {
+      "name": "YOYO",
+      'has_pets': True
+    }
+  
+
+  def __str__(self):
+    return f'{self.color}'
+
+  def __len__(self):
+    return 5
+
+  def __call__(self):
+    return("yesss??")
+
+  def __getitem__(self, i):
+    return self.my_dict[i]
+
+
+action_figure = Toy('red', 0)
+print(action_figure.__str__()) #red
+print(str("action_figure")) #action_figure
+print(len(action_figure)) #5
+print(action_figure()) # __call__ will print "yesss??"
+print(action_figure['name'])
